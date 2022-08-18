18374635c2088c5dbcd7b4ade4e6c9cbe4455bd5
Emily C. Wilke <emilycwilke@gmail.com>
2022-08-19 00:50:40 +0200

    fixing some early mistakes

--- custom.py
+++ custom.py
@@ -7,13 +7,16 @@ def convert_to_csv(file_path):
     return dataframe
 
 def convert_stop_locations(dataframe):
-    stops_geoframe = gp.GeoDataFrame(dataframe, geometry=gp.points_from_xy(dataframe.stop_lat, dataframe.stop_lon))
-    return stops_geoframe
+    #converting pd.DataFram to gp.GeoDataFrame, and setting the Coordinate Reference System to WGS 84
+    stops_gdf = gp.GeoDataFrame(dataframe, geometry=gp.points_from_xy(dataframe.stop_lon, dataframe.stop_lat)).set_crs(epsg=4326, inplace=True)
+    return stops_gdf
 
 def main():
-    stops_geoframe = convert_stop_locations(convert_to_csv("2022-07-26_RNV_GTFS_Schedule\\stops.txt"))
-    print(stops_geoframe.head())
-    stops_geoframe.to_file("stops.shp")
+    stops_gdf = convert_stop_locations(convert_to_csv("2022-07-26_RNV_GTFS_Schedule\\stops.txt"))
+    stops_gdf.head()
+
+    #exporting a shp-file for use in a Desktop GIS environment
+    stops_gdf.to_file("stops.shp")
 
 if __name__ == "__main__":
     main()
